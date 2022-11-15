27b26ecb6a5e788bb70b560002c1c8ac28b7e5d6
Kenneth Chien <kennethchien.kc@gmail.com>
2022-11-16 02:05:37 +0800

    Added artwork controller

---
+++ app/controllers/artworks_controller.rb
@@ -0,0 +1,16 @@
+class ArtworksController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index show]
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+
+  end
+end

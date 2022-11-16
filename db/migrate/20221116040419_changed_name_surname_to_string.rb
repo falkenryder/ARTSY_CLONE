555dc1887380434b939e31ae37d42efe8ed8e95c
Michael Thomas <mike.ahpong@gmail.com>
2022-11-16 14:24:57 +0800

    create username and surname with type string

---
+++ db/migrate/20221116040419_changed_name_surname_to_string.rb
@@ -0,0 +1,4 @@
+class ChangedNameSurnameToString < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
--- db/migrate/20221116061746_add_username_lastname_to_users.rb
+++ db/migrate/20221116061746_add_username_lastname_to_users.rb
@@ -1,4 +1,4 @@
-class AddNameSurnametoUsers < ActiveRecord::Migration[7.0]
+class AddUsernameLastnameToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column(:users, :first_name, :string, null: false)
     add_column(:users, :last_name, :string, null: false)

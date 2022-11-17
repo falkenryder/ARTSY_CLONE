cee5a0d16962c072d41157107b76980a4ada8a73
Kenneth Chien <kennethchien.kc@gmail.com>
2022-11-17 12:46:45 +0800

    Addedlink to view seller artworks on homepage

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,13 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+
   resources :artworks, only: %i[index show new create destroy] do
     resources :offers, only: %i[new create]
+  end
+
+  resources :users do
+    resources :artworks
   end
 
   resources :offers, only: %i[index show]

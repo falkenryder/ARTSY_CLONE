37345741fc2921938e6fc64575b895626af09d6c
christina choi <christina.yk.choi@gmail.com>
2022-11-21 13:47:14 +0800

    front end and price thousand separator

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,4 +1,6 @@
 class Artwork < ApplicationRecord
+  include ActionView::Helpers::NumberHelper
+
   validates :artist_name, :title, :theme, :year, :price, :details, presence: true
   validates :theme, inclusion: { in: %w[Contemporary Street-Art Pop-Art Abstract-Expressionism Post-War Impressionism Old-Masters] }
 
@@ -6,4 +8,9 @@ class Artwork < ApplicationRecord
 
   # belongs_to :owner, class_name: "User"
   has_many :offers, dependent: :destroy
+
+  def price_delimit
+    return "#{number_with_delimiter(self.price)}"
+  end
+
 end
